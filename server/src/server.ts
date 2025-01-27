03b8bec9c0d56d12ef10ea155e498c71e70fca1f
KeelerZoroth <isaiah1hansen@gmail.com>
2025-01-27 14:53:47 -0600

    updated vite config and commented out GraphQL code untill it is implemented

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,19 +9,19 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
-import { expressMiddleware } from '@apollo/server/express4';
-import { typeDefs, resolvers } from './schemas/index.js';
-import { authenticateToken } from './utils/auth.js';
+// import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
+// import { expressMiddleware } from '@apollo/server/express4';
+// import { typeDefs, resolvers } from './schemas/index.js';
+// import { authenticateToken } from './utils/auth.js';
 
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers
+// });
 
 const startApolloServer = async () => {
-  await server.start();
+  // await server.start();
   await db();
 
   const PORT = process.env.PORT || 3001;
@@ -30,11 +30,11 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
-  app.use('/graphql', expressMiddleware(server as any,
-    {
-      context: authenticateToken as any
-    }
-  ));
+  // app.use('/graphql', expressMiddleware(server as any,
+  //   {
+  //     context: authenticateToken as any
+  //   }
+  // ));
 
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../../client/dist')));
