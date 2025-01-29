0c4d5d3de6e490d09bb5beb395b357be836a5643
jclark-bcamp <jnssclark@gmail.com>
2025-01-28 18:24:08 -0600

    login/signup, models, api.ts - needs work

---
+++ client/src/models/Snippet.ts
@@ -0,0 +1,9 @@
+export interface Snippet {
+    _id: string;
+    author: string;
+    title: string;
+    summary: string;
+    text: string;
+    createdAt: string;
+  }
+ 
---
+++ client/src/models/Users.ts
@@ -0,0 +1,10 @@
+// import type {  } from '';
+
+
+export interface User {
+    username: string | null;
+    email: string | null;
+    password: string | null;
+    savedSnippet: [];
+   }
+   
