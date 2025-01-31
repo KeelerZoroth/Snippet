6266ada123c31e89738dfd6afe6299bef6e19631
KeelerZoroth <132631507+KeelerZoroth@users.noreply.github.com>
2025-01-30 20:33:39 -0600

    Update Navbar.tsx

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,18 +5,13 @@ import styled, { css } from 'styled-components'
 import TempSnippetLogo from '../../public/TempSnippetLogo.png';
 
 
-const Button = styled.button<{ primary: boolean }>`
+const Button = styled.button`
       background: transparent;
   border-radius: 3px;
   border: 2px solid #BF4F74;
   color: #BF4F74;
   margin: 0.5em 1em;
   padding: 0.25em 1em;
-
-  ${props => props.primary && css`
-    background: #BF4F74;
-    color: white;
-  `}
 `;
 
 const Container = styled.div`
